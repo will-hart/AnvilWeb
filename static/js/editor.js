49d5f28509477e2d27c00c853ef99217b2367590
William Hart <hart.wl@gmail.com>
2013-03-16 19:00:20 +0000

    Update editor styles

--- static/js/editor.js
+++ static/js/editor.js
@@ -1,4 +1,10 @@
 /* start the editor from the textarea */
 window.onload = function() {
-    var cmEditor = CodeMirror.fromTextArea(document.getElementById("editor"));
+    var cmEditor = CodeMirror.fromTextArea(document.getElementById("editor"),{
+        indentUnit: 4,
+        lineNumbers: true,
+        theme: 'ambiance'
+    }).setSize("100%", "600");
+    
+    // set up the editor
 }
