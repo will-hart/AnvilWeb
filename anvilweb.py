cf91cfaa548922bd4485dd4197cbaed21ebbb715
William Hart <hart.wl@gmail.com>
2013-03-29 00:00:59 +0000

    Add v2 download

--- anvilweb.py
+++ anvilweb.py
@@ -157,7 +157,8 @@ class HomeHandler(BaseHandler):
 class DownloadHandler(BaseHandler):
     def get(self):
         d001 = self.db.get("SELECT downloads FROM files WHERE file_slug='0.1';")
-        self.render("download.html", d001=d001.downloads)
+        d002 = self.db.get("SELECT downloads FROM files WHERE file_slug='0.2';")
+        self.render("download.html", d001=d001.downloads, d002=d002.downloads)
 
 
 class GalleryHandler(BaseHandler):
